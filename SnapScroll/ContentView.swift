ef9c0c3a56588093118d4a3b5e480d45e3c4e5de
Naveen Reddy Nagulapally <naveenreddynagulapally@Naveens-MacBook-Air.local>
2024-10-21 13:32:30 -0400

    initial commit

---
+++ SnapScroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapScroll
+//
+//  Created by Naveen Reddy Nagulapally on 2024-10-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
